89551f04adec9d6087b8cfa1d17da5e26232a7d7
adamzermane <adamzermane92@yahoo.com>
2022-08-16 17:22:40 +0700

    added a time functionality as well as improved the post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Layout from '../../components/layout';
 import {getAllPostIds, getPostData} from '../../lib/posts';
 import Head from 'next/head'; 
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css'; 
 
 
 export default function Post({postData}) {
@@ -9,16 +11,22 @@ export default function Post({postData}) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            {postData.title}
+           <Date dateString={postData.date}/>
+           <article>
+
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            <div className={utilStyles.lightText}></div>
             <br />
             {postData.id}
-            <br/>
-            {postData.date}
-            <br />
+        
             <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+
+           </article>
         </Layout>
     ); 
 }
+
+
 
 export async function getStaticPaths() {
     const paths = getAllPostIds()
